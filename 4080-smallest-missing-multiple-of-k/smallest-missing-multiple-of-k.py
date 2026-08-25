b910c37ba35d23f861085f04af038858de3a6bc8
Abhinand G <abhinand.g04@gmail.com>
2026-08-25 21:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.35%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -3,7 +3,5 @@ class Solution:
         i=1
         h=set(nums)
         while True:
-            if i*k not in h:
-                return i*k
-            i+=1
-            
+            if i*k not in h:return i*k
+            i+=1    
